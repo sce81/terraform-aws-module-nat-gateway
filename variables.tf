b85f3ab24a96006100bce97701561582b284dd09
Simon Elliott <simon.elliott@slalom.com>
2023-04-24 13:44:49 +0100

    develop - initial commit

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "nat_gateway_count"            {default = 3}
-variable "env"                          {}
+variable "env_name"                          {}
 variable "vpc_id"                       {}
 
 variable "common_tags"                  {
