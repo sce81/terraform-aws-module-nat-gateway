7462424ac27fd6d9064c9fb908fcd0de71bc0344
Simon Elliott <simon.elliott@slalom.com>
2023-04-24 14:13:34 +0100

    develop - converted to TFC

--- main.tf
+++ main.tf
@@ -1,26 +1,26 @@
 resource "aws_nat_gateway" "main" {
-    // 1 per vpc for costs, 1 per az for resilience
-    count               = var.nat_gateway_count
-    allocation_id       = element(aws_eip.main[*].id, count.index)
-    subnet_id           = element(local.subnet_ids, count.index)
+  // 1 per vpc for costs, 1 per az for resilience
+  count         = var.nat_gateway_count
+  allocation_id = element(aws_eip.main[*].id, count.index)
+  subnet_id     = element(local.subnet_ids, count.index)
 
   tags = merge(
     local.common_tags, var.extra_tags,
     tomap({
-      Name = "${var.vpc_id}-${var.env_name}-natgw${count.index +1}"
+      Name = "${var.vpc_id}-${var.env_name}-natgw${count.index + 1}"
     })
   )
 
 }
 
 resource "aws_eip" "main" {
-    count               = var.nat_gateway_count
-    vpc                 = true
+  count = var.nat_gateway_count
+  vpc   = true
 
   tags = merge(
     local.common_tags, var.extra_tags,
     tomap({
-      Name = "${var.vpc_id}-${var.env_name}-natgw${count.index +1}-eip"
+      Name = "${var.vpc_id}-${var.env_name}-natgw${count.index + 1}-eip"
     })
   )
 } 
--- variables.tf
+++ variables.tf
@@ -1,12 +1,14 @@
-variable "nat_gateway_count"            {default = 3}
-variable "env_name"                          {}
-variable "vpc_id"                       {}
+variable "nat_gateway_count" { default = 3 }
 
-variable "common_tags"                  {
-                                        type = map
-                                        default = {}
-                                        }
-variable "extra_tags"                   {
-                                        type = map
-                                        default = {}
-                                        }
+variable "name" {}
+variable "env_name" {}
+variable "vpc_id" {}
+
+variable "common_tags" {
+  type    = map(any)
+  default = {}
+}
+variable "extra_tags" {
+  type    = map(any)
+  default = {}
+}
